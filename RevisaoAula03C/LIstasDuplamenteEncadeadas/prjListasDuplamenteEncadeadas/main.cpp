dff06bffd56aad7edd42bf29ae1d830d2a4701f7
gusmezomo <gusmezomo@gmail.com>
2025-03-31 21:01:05 -0300

    teste 2

--- RevisaoAula03C/LIstasDuplamenteEncadeadas/prjListasDuplamenteEncadeadas/main.cpp
+++ RevisaoAula03C/LIstasDuplamenteEncadeadas/prjListasDuplamenteEncadeadas/main.cpp
@@ -1,203 +1,173 @@
-// LISTA DUPLAMENTE ENCADEADA
-#include <stdio.h>
-#include <stdlib.h>
+#include <iostream>
 #include <cstdlib>
+using namespace std;
 
-typedef struct elemento Elemento;
-struct elemento
-{
-    int valor;
-    Elemento *proximo;
-    Elemento *anterior;
+class ListaDuplamenteEncadeada {
+private:
+    struct Elemento {
+        int valor;
+        Elemento* proximo;
+        Elemento* anterior;
+    };
+
+    Elemento* inicio;
+    Elemento* fim;
+    int tamanho;
+
+    Elemento* alocarMemoria(int valor) {
+        Elemento* novo = new Elemento;
+        novo->valor = valor;
+        novo->proximo = nullptr;
+        novo->anterior = nullptr;
+        return novo;
+    }
+
+public:
+    ListaDuplamenteEncadeada() {
+        inicio = nullptr;
+        fim = nullptr;
+        tamanho = 0;
+    }
+
+    ~ListaDuplamenteEncadeada() {
+        while (inicio != nullptr) {
+            Elemento* atual = inicio;
+            inicio = inicio->proximo;
+            delete atual;
+        }
+    }
+
+    void removerInicio() {
+        if (inicio != nullptr) {
+            Elemento* atual = inicio;
+            inicio = atual->proximo;
+
+            if (inicio != nullptr)
+                inicio->anterior = nullptr;
+            else
+                fim = nullptr;
+
+            delete atual;
+            tamanho--;
+        }
+    }
+
+    void removerFim() {
+        if (fim != nullptr) {
+            Elemento* atual = fim;
+            fim = atual->anterior;
+
+            if (fim != nullptr)
+                fim->proximo = nullptr;
+            else
+                inicio = nullptr;
+
+            delete atual;
+            tamanho--;
+        }
+    }
+
+    void inserirInicio(int valor) {
+        Elemento* novo = alocarMemoria(valor);
+
+        if (inicio == nullptr) {
+            inicio = fim = novo;
+        } else {
+            novo->proximo = inicio;
+            inicio->anterior = novo;
+            inicio = novo;
+        }
+
+        tamanho++;
+    }
+
+    void inserirFim(int valor) {
+        Elemento* novo = alocarMemoria(valor);
+
+        if (inicio == nullptr) {
+            inicio = fim = novo;
+        } else {
+            novo->anterior = fim;
+            fim->proximo = novo;
+            fim = novo;
+        }
+
+        tamanho++;
+    }
+
+    void inserirPosicao(int valor, int posicao) {
+        posicao = posicao - 1;
+        Elemento* novo = alocarMemoria(valor);
+
+        if (inicio == nullptr) {
+            inicio = fim = novo;
+        } else {
+            Elemento* atual;
+            int i;
+
+            if (posicao < (tamanho / 2)) {
+                atual = inicio;
+                i = 0;
+                while (i < posicao && atual != nullptr) {
+                    atual = atual->proximo;
+                    i++;
+                }
+            } else {
+                atual = fim;
+                i = tamanho - 1;
+                while (i > posicao && atual != nullptr) {
+                    atual = atual->anterior;
+                    i--;
+                }
+            }
+
+            if (atual != nullptr) {
+                novo->anterior = atual->anterior;
+                novo->proximo = atual;
+                if (atual->anterior != nullptr)
+                    atual->anterior->proximo = novo;
+                else
+                    inicio = novo;
+
+                atual->anterior = novo;
+                tamanho++;
+
+                cout << "Valor antecessor ao que desejo inserir: " << (novo->anterior ? novo->anterior->valor : -1) << endl;
+                cout << "Valor proximo ao que desejo inserir: " << (novo->proximo ? novo->proximo->valor : -1) << endl;
+            }
+        }
+    }
+
+    void imprimirLista() {
+        Elemento* atual = inicio;
+        while (atual) {
+            cout << atual->valor << endl;
+            atual = atual->proximo;
+        }
+    }
+
+    void imprimirListaInversa() {
+        Elemento* atual = fim;
+        while (atual) {
+            cout << atual->valor << endl;
+            atual = atual->anterior;
+        }
+    }
 };
 
-// Criar 2 vari�veis globais.
-Elemento *inicio;
-Elemento *fim;
-int tamanho;
-
-// Criar 2 elemmentos nulos na lista.
-void criarLista()
-{
-    inicio = NULL;
-    fim = NULL;
-    tamanho = 0;
-}
-
-// Alocar um dado, e como funciona o esquema do lista duplamente encadeada:
-Elemento *alocarMemoria(int valor)
-{
-    //Aloca��o de mem�ria p/ pr�ximo elemento
-    Elemento *novo = (Elemento *)malloc(sizeof(Elemento));
-
-    novo -> valor = valor;
-    novo -> anterior = NULL;
-    novo -> proximo = NULL;
-
-    return novo;
-}
-
-void removerInicio()
-{
-    Elemento *atual = inicio;
-
-    inicio = atual->proximo;
-    inicio->anterior = NULL;
-    tamanho--;
-}
-
-void removerFim()
-{
-    Elemento *atual = fim;
-
-    fim = atual->anterior;
-    fim->proximo = NULL;
-    tamanho--;
-}
-
-// Inserir no inicio da lista:
-void inserirInicio(int valor)
-{
-    Elemento *novo = alocarMemoria(valor); // criar novo elemento alocando mem�ria
-
-    // verificar se o inicio for nulo, se sim, inicio recebe novo e o fim vai ser novo tamb�m, pois se os dois s�o nulos o inico e fim recebe o novo.
-    if (inicio == NULL)
-    {
-        inicio = novo;
-        fim = novo;
-    }
-    else
-    {
-        Elemento *atual; // criar novo elemento caso tenha dado no inicio da lista.
-
-        atual = inicio;
-        novo->proximo = atual;
-        atual->anterior = novo;
-        inicio = novo;
-    }
-
-    tamanho++;
-}
-
-// Inserir no fim da lista:
-void inserirFim(int valor)
-{
-    Elemento *novo = alocarMemoria(valor); // criar novo elemento alocando mem�ria
-
-    // verificar se o inicio for nulo, se sim, inicio recebe novo e o fim vai ser novo tamb�m, pois se os dois s�o nulos o inico e fim recebe o novo.
-    if (inicio == NULL)
-    {
-        inicio = novo;
-        fim = novo;
-    }
-    else
-    {
-        Elemento *atual;
-
-        atual = fim;
-        novo->anterior = atual;
-        atual->proximo = novo;
-        fim = novo;
-    }
-
-    tamanho++;
-}
-
-// Inserir no inicio da lista:
-void inserirPosicao(int valor, int posicao)
-{
-    posicao = posicao - 1;
-    Elemento *novo = alocarMemoria(valor); // criar novo elemento alocando mem�ria
-
-    // verificar se o inicio for nulo, se sim, inicio recebe novo e o fim vai ser novo tamb�m, pois se os dois s�o nulos o inico e fim recebe o novo.
-    if (inicio == NULL)
-    {
-        inicio = novo;
-        fim = novo;
-
-    }
-    else
-    {
-        Elemento *atual; // criar novo elemento caso tenha dado no inicio da lista.
-        Elemento *tmp;
-        int i;
-
-        // Decidindo o ponto de incio
-        if (posicao < (tamanho / 2))
-        {
-            atual = inicio;
-            i = 0;
-        }
-        else
-        {
-            atual = fim;
-            i = tamanho;
-        }
-
-        // Percorrer para encontrar a posi��o
-        while ((i != posicao) && (i >= 0) && (i <= tamanho))
-        {
-            if (i < posicao)
-            {
-                i++;
-                atual = atual->proximo;
-            }
-            else if (i > posicao)
-            {
-                i--;
-                atual = atual->anterior;
-            }
-
-            if (posicao == i)
-            {
-                printf("Valor antecessor ao que desejo inserir %d\n", atual->anterior->valor);
-                printf("Valor proximo ao que desejo inserir %d\n", atual->proximo->valor);
-            }
-        }
-    }
-
-}
-
-// Fun��o para imprimir a lista:
-void imprimirLista()
-{
-    Elemento *atual;
-    atual = inicio;
-
-    while (atual)
-    {
-        printf("%d\n", atual->valor);
-        atual = atual->proximo;
-    }
-}
-
-// Fun��o para imprimir a lista:
-void imprimirListaInversa()
-{
-    Elemento *atual;
-    atual = fim;
-
-    while (atual)
-    {
-        printf("%d\n", atual->valor);
-        atual = atual->anterior;
-    }
-}
-
-int main()
-{
-    //Criar a lista em mem�ria
-    criarLista();
+int main() {
+    ListaDuplamenteEncadeada lista;
 
     for (int i = 0; i < 10; i++)
-        inserirInicio(rand());
+        lista.inserirInicio(rand());
 
-    //Inserir na posi��o espec�fica
-    inserirPosicao(1000, 2);
+    lista.inserirPosicao(1000, 2);
 
-    printf("Imprimir lista atual ...\n");
-    imprimirLista();
+    cout << "Imprimir lista atual ..." << endl;
+    lista.imprimirLista();
 
-    //printf("Imprimir lista inversa ...\n");
-    //imprimirListaInversa();
+    // cout << "Imprimir lista inversa ..." << endl;
+    // lista.imprimirListaInversa();
+
+    return 0;
 }
